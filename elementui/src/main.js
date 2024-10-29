c401d4ef0a52d26ca5055cae364958c95444a59c
name-is-xxx <hhhuanglin@gmail.com>
2024-10-29 19:17:23 +0800

    111

--- elementui/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.full";
-
-createApp(App).use(ElementPlus).mount("#app");
